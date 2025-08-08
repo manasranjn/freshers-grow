3213247e5d14a62ea309ccbbc906485fc4431048
Manas Ranjan Bera <manasbera484@gmail.com>
2025-08-08 14:25:35 +0530

    init

---
+++ client/src/routes/middleware/NonAuthLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NonAuthLayout = () => {
+  return (
+    <div>
+      <h1>NonAuthLayout</h1>
+    </div>
+  );
+};
+
+export default NonAuthLayout;
---
+++ client/src/routes/middleware/UserLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserLayout = () => {
+  return (
+    <div>
+      <h1>UserLayout</h1>
+    </div>
+  );
+};
+
+export default UserLayout;
